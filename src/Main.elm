e8eb5b0125919a76dc70d6553d426c07a8baff18
George Wilson <george@wils.online>
2019-09-05 15:25:35 +1000

    exercise 2

--- src/Main.elm
+++ src/Main.elm
@@ -27,38 +27,52 @@ type Msg
     = HandleLoginResp (Result Http.Error String)
     | SetLoginPlayerId String
     | SetLoginPassword String
+    | SetRegisterPlayerId String
+    | SetRegisterPassword String
+    | SetRegisterPasswordAgain String
     | LoginSubmit
+    | RegisterSubmit
 
 
 type alias Model =
     { backendOK : Bool
-    , backendError : Maybe String
+    , loginError : Maybe String
     , loginPlayerId : String
     , loginPassword : String
-    , token : Maybe String
+    , loginToken : Maybe String
+    , registerPlayerId : String
+    , registerPassword : String
+    , registerPasswordAgain : String
+    , registerToken : Maybe String
+    , registerValidationIssues : List String
     }
 
 
 init : flags -> ( Model, Cmd Msg )
 init _ =
     ( { backendOK = True
-      , backendError = Nothing
+      , loginError = Nothing
       , loginPlayerId = ""
       , loginPassword = ""
-      , token = Nothing
+      , loginToken = Nothing
+      , registerPlayerId = ""
+      , registerPassword = ""
+      , registerPasswordAgain = ""
+      , registerToken = Nothing
+      , registerValidationIssues = []
       }
-    , BE.postApiLogin (BE.DbPlayer "user1" "pass") HandleLoginResp
+    , Cmd.none
     )
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
 update action model =
     case action of
-        HandleLoginResp (Ok r) ->
-            ( { model | backendOK = True, backendError = Nothing, token = Just r }, Cmd.none )
+        HandleLoginResp (Ok token) ->
+            ( { model | backendOK = True, loginError = Nothing, loginToken = Just token }, Cmd.none )
 
         HandleLoginResp (Err err) ->
-            ( { model | backendError = Just "Backend login failed", backendOK = False }, Cmd.none )
+            ( { model | loginError = Just "Backend login failed", backendOK = False }, Cmd.none )
 
         SetLoginPlayerId pid ->
             ( { model | loginPlayerId = pid }, Cmd.none )
@@ -66,8 +80,25 @@ update action model =
         SetLoginPassword pwd ->
             ( { model | loginPassword = pwd }, Cmd.none )
 
+        SetRegisterPlayerId pid ->
+            ( { model | registerPlayerId = pid }, Cmd.none )
+
+        SetRegisterPassword pwd ->
+            ( { model | registerPassword = pwd }, Cmd.none )
+
+        SetRegisterPasswordAgain pwd ->
+            ( { model | registerPasswordAgain = pwd }, Cmd.none )
+
         LoginSubmit ->
             ( model, BE.postApiLogin (BE.DbPlayer model.loginPlayerId model.loginPassword) HandleLoginResp )
+
+        RegisterSubmit ->
+            case validateDbPlayer model of
+                Err problems ->
+                    ( { model | registerValidationIssues = problems, registerToken = Nothing }, Cmd.none )
+
+                Ok dbPlayer ->
+                    ( { model | registerValidationIssues = [], registerToken = Nothing }, BE.postApiPlayers dbPlayer HandleLoginResp )
 
 
 subscriptions : Model -> Sub Msg
@@ -77,29 +108,71 @@ subscriptions model =
 
 view : Model -> H.Html Msg
 view model =
-    case model.backendError of
+    case model.loginError of
         Just err ->
             H.div [] [ H.h1 [] [ H.text "OH NOES" ] ]
 
         Nothing ->
-            H.div [ HA.class "login-box" ]
-                [ H.h1 [] [ H.text "Login" ]
-                , H.form [ HE.onSubmit LoginSubmit ]
-                    [ H.input
-                        [ HA.placeholder "Player Id"
-                        , HAA.ariaLabel "Player ID"
-                        , HE.onInput SetLoginPlayerId
+            H.div []
+                [ H.div [ HA.class "login-box" ]
+                    [ H.h1 [] [ H.text "Login" ]
+                    , H.form [ HE.onSubmit LoginSubmit ]
+                        [ H.input
+                            [ HA.placeholder "Player Id"
+                            , HAA.ariaLabel "Player ID"
+                            , HE.onInput SetLoginPlayerId
+                            ]
+                            []
+                        , H.input
+                            [ HA.placeholder "Password"
+                            , HA.type_ "password"
+                            , HAA.ariaLabel "Password"
+                            , HE.onInput SetLoginPassword
+                            ]
+                            []
+                        , H.button
+                            [ HA.class "btn primary" ]
+                            [ H.text "Login" ]
                         ]
-                        []
-                    , H.input
-                        [ HA.placeholder "Password"
-                        , HA.type_ "password"
-                        , HAA.ariaLabel "Password"
-                        , HE.onInput SetLoginPassword
+                    ]
+                , H.div [ HA.class "login-box" ]
+                    [ H.h1 [] [ H.text "Register" ]
+                    , H.form [ HE.onSubmit RegisterSubmit ]
+                        [ H.input
+                            [ HA.placeholder "Player Id"
+                            , HAA.ariaLabel "Player ID"
+                            , HE.onInput SetRegisterPlayerId
+                            ]
+                            []
+                        , H.input
+                            [ HA.placeholder "Password"
+                            , HA.type_ "password"
+                            , HAA.ariaLabel "Password"
+                            , HE.onInput SetRegisterPassword
+                            ]
+                            []
+                        , H.input
+                            [ HA.placeholder "Password again"
+                            , HA.type_ "password"
+                            , HAA.ariaLabel "Password again"
+                            , HE.onInput SetRegisterPasswordAgain
+                            ]
+                            []
+                        , H.button
+                            [ HA.class "btn primary" ]
+                            [ H.text "Login" ]
                         ]
-                        []
-                    , H.button
-                        [ HA.class "btn primary" ]
-                        [ H.text "Login" ]
                     ]
                 ]
+
+
+validateDbPlayer : Model -> Result.Result (List String) BE.DbPlayer
+validateDbPlayer model =
+    if model.registerPassword == "" then
+        Err [ "password is empty" ]
+
+    else if model.registerPassword /= model.registerPasswordAgain then
+        Err [ "passwords do not match" ]
+
+    else
+        Ok (BE.DbPlayer model.registerPlayerId model.registerPassword)
